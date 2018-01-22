a9ac9b3b0756b1c233c19819ec0401e1a8b759bf
Dat Tran <trphatdat@gmail.com>
2018-01-22 14:03:49 +0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by DatTran on 1/19/18.
+//  Copyright © 2018 DatTran. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done:Bool = false
+    
+}
